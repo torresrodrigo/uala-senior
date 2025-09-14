e74cc7dbd6a027cc5c197a8fd6a230d433160804
torresrodrigo <33295900+torresrodrigo@users.noreply.github.com>
2025-09-14 14:41:10 -0300

    [FEAT-01] Setup Project

---
+++ City-Exploration/City-Exploration/City_ExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_ExplorationApp.swift
+//  City-Exploration
+//
+//  Created by Rodrigo Torres on 14/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct City_ExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
